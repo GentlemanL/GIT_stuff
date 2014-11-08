f8030647995f5387248dedd35e6c95a336e20a52
GentlemanL <lukasksenzuliak@gmail.com>
2014-11-08 23:13:26 +0100

    fcie
    snad spravene 3 funkcie meniace obsah

--- IReSoft_priklad_v2/IReSoft_priklad_v2/RemoveSpacesAndPunctuation.cs
+++ IReSoft_priklad_v2/IReSoft_priklad_v2/RemoveSpacesAndPunctuation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,43 +12,42 @@ namespace IReSoft_priklad_v2
 
         public override string Run(string s)
         {
-            string x = string.Empty;
-            char tmp = x[0];
+            string returnString = string.Empty;
+            char tmp = s[0];
 
-            //iba jedna operacia - ais nepouzitelne
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            textInfo.ToTitleCase(s);
+
+            //iba jedna operacia - asi nepouzitelne
             //string x = new string(s.Where(c => !char.IsPunctuation(c) || char.IsWhiteSpace(c)).ToArray());
+
             for (int i = 0; i < s.Length; i++)
             {
-                if (!char.IsPunctuation(s[i]) && !char.IsWhiteSpace(s[i]))
+                if (!char.IsPunctuation(s[i]) && !(s[i] == ' '))
                 {
-                    x += s[i];
+                    returnString += s[i];
                     if (i > 1) { tmp = s[i - 1]; }
                 }
                 setProgres(s[i], tmp);
             }
-            return x;
+            return returnString;
         }
 
-        protected override void setProgres(char currChar, char prevChar)
-        {
-            //'\n' je white space treba osetrit aby sem prisiel a neodstranoval ho asi
-            lock (pr)
-            {
-                pr.numOfChars++;
-                if (currChar == '.' || currChar == '?' || currChar == '!')
-                {
-                    pr.numOfSentences++;
-                }
-                if (currChar == '\n')
-                {
-                    pr.numOfLines++;
-                }
-                if (currChar == ' ' && !(currChar == ' '))
-                {
-                    pr.numOfWords++;
-                }
-            }
-        }
+        //protected override void setProgres(char currChar, char prevChar)
+        //{
+        //    lock (pr)
+        //    {
+        //        pr.numOfChars++;
+        //        if (currChar == '.' || currChar == '?' || currChar == '!')
+        //        {
+        //            pr.numOfSentences++;
+        //        }
+        //        if (currChar == '\n')
+        //        {
+        //            pr.numOfLines++;
+        //        }
+        //    }
+        //}
 
     }
 }
